e033904c568e6191745c28d617e13e01ef14843b
aniket umbc <aniket.umbc@gmail.com>
2018-03-31 17:18:36 +0530

    anonymous function with event

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var scores, roundScore ,activePlayer;
 
  scores = [0,0];
  roundScore = 0;
- activePlayer = 1;
+ activePlayer = 0;
 //document.querySelector('#current-' + activePlayer).textContent = dice; 
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
@@ -26,13 +26,37 @@ document.getElementById('current-0').textContent = 0;
 document.getElementById('current-1').textContent = 0;
 
 document.querySelector('.btn-roll').addEventListener('click',function (){
-    
-    //random number
+        //random number
     var  dice =  Math.floor(Math.random() * 6 ) + 1;
     // display result
     var diceDom = document.querySelector('.dice');
     diceDom.style.display = 'block';
     diceDom.src = 'dice-' + dice + '.png';
+
+    if (dice !== 1) {
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+        
+
+        // document.querySelector('.player-0-panel').classList.remove('active');
+        // document.querySelector('.player-1-panel').classList.add('active');
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        document.querySelector('.dice').style.display = 'none';
+
+    }
+
+
+
+
     });
 
 
