852b3eea654f4f73163a4f4c68ca83e3e3503ab0
aniket umbc <aniket.umbc@gmail.com>
2018-03-31 18:36:47 +0530

    Added State variable

--- app.js
+++ app.js
@@ -7,42 +7,46 @@ GAME RULES:
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
-*/   
-var scores, roundScore, activePlayer;
+*/
+var scores, roundScore, activePlayer, gamePlaying;
 init();
 document.getElementById('current-1').textContent = 0;
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
-    //random number
-    var dice = Math.floor(Math.random() * 6) + 1;
-    // display result
-    var diceDom = document.querySelector('.dice');
-    diceDom.style.display = 'block';
-    diceDom.src = 'dice-' + dice + '.png';
+    if (gamePlaying) {
+        //random number
+        var dice = Math.floor(Math.random() * 6) + 1;
+        // display result
+        var diceDom = document.querySelector('.dice');
+        diceDom.style.display = 'block';
+        diceDom.src = 'dice-' + dice + '.png';
 
-    if (dice !== 1) {
-        roundScore += dice;
-        document.querySelector('#current-' + activePlayer).textContent = roundScore;
-    } else {
-        nextPlayer();
+        if (dice !== 1) {
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        } else {
+            nextPlayer();
+        }
     }
 });
 document.querySelector('.btn-hold').addEventListener('click', function () {
-    // add current score to global score 
-    scores[activePlayer] += roundScore;
-    //update UI 
-    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];   
-    
-    if(scores[activePlayer] >= 10){
-     document.querySelector('#name-' +activePlayer).textContent = 'Winner';
-     document.querySelector('.dice').style.display ='none';
-     document.querySelector('.player-' +activePlayer + '-panel').classList.add('winner');
-     document.querySelector('.player-' +activePlayer + '-panel').classList.remove('active');
-    }else{
-        nextPlayer();
-    }   
+    if (gamePlaying) {
+        // add current score to global score 
+        scores[activePlayer] += roundScore;
+        //update UI 
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+        if (scores[activePlayer] >= 10) {
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            gamePlaying = false;
+        } else {
+            nextPlayer();
+        }
+    }
 });
-document.querySelector('.btn-new').addEventListener('click',init);
+document.querySelector('.btn-new').addEventListener('click', init);
 function nextPlayer() {
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     roundScore = 0;
@@ -58,6 +62,7 @@ function init() {
     scores = [0, 0];
     roundScore = 0;
     activePlayer = 0;
+    gamePlaying = true;
     document.querySelector('.dice').style.display = 'none';
     document.getElementById('score-1').textContent = 0;
     document.getElementById('score-0').textContent = 0;
@@ -69,9 +74,9 @@ function init() {
     document.querySelector('.player-1-panel').classList.remove('winner');
     document.querySelector('.player-0-panel').classList.remove('active');
     document.querySelector('.player-1-panel').classList.remove('active');
-    document.querySelector('.player-0-panel').classList.add ('active');
-  }
-  
+    document.querySelector('.player-0-panel').classList.add('active');
+}
+
 
 
 
